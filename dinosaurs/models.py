a7e9d11b194c0c375726f032c9b77b6be2764f01
code043 <procoder043@gmail.com>
2024-11-28 19:45:06 -0300

    Dino Model

--- dinosaurs/models.py
+++ dinosaurs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Dino(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.URLField(max_length=200)  
+    period = models.CharField(max_length=50)
+    diet = models.CharField(max_length=20)
+    size = models.CharField(max_length=50)
+    weight = models.CharField(max_length=50)
+    habitat = models.CharField(max_length=200)
+    characteristics = models.TextField() 
