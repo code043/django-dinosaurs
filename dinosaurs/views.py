fc891287f64c4ae8f1ea29c0fb83c64546d80f58
code043 <procoder043@gmail.com>
2024-11-28 21:20:04 -0300

    Add a new Dino

--- dinosaurs/views.py
+++ dinosaurs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Dino
 
 # Create your views here.
 def home(request):
   return render(request, "home.html")
+
+def dinos(request):
+  dinos = Dino.objects.all()
+  return render(request,"dinos.html",{"dinos": dinos})
